a803af6f084881096a78b65a8c58958746673954
Ausath Ikram <ausathdzil@gmail.com>
2025-11-30 21:08:11 +0700

    Add item component, update home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,61 @@
-import { Text } from '@/components/typography';
+import { MoreHorizontalIcon } from 'lucide-react';
+import { Suspense } from 'react';
 
-export default async function Home() {
+import {
+  Empty,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemTitle,
+} from '@/components/ui/item';
+import { elysia } from '@/lib/eden';
+
+export default function Home() {
   return (
-    <main className="grid flex-1 place-items-center">
-      <Text>…</Text>
+    <main className="mx-auto grid w-full max-w-4xl flex-1">
+      <section className="w-full p-4">
+        <Suspense fallback={null}>
+          <Articles />
+        </Suspense>
+      </section>
     </main>
+  );
+}
+
+async function Articles() {
+  const articles = await elysia.articles.get();
+
+  if (articles.data?.length === 0) {
+    return (
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <MoreHorizontalIcon />
+          </EmptyMedia>
+          <EmptyTitle>No articles yet…</EmptyTitle>
+        </EmptyHeader>
+      </Empty>
+    );
+  }
+
+  return (
+    <ItemGroup className="list-none gap-4">
+      {articles.data?.map((article) => (
+        <li key={article.publicId}>
+          <Item variant="muted">
+            <ItemContent>
+              <ItemTitle>{article.title}</ItemTitle>
+              <ItemDescription>{article.excerpt}</ItemDescription>
+            </ItemContent>
+          </Item>
+        </li>
+      ))}
+    </ItemGroup>
   );
 }
