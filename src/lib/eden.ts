fdae5010f1d03d9d888b9ece8177e7ccd5014090
Ausath Ikram <ausathdzil@gmail.com>
2026-02-12 14:55:14 +0700

    fix: remove env check in eden

--- src/lib/eden.ts
+++ src/lib/eden.ts
@@ -2,9 +2,7 @@ import { treaty } from '@elysiajs/eden';
 
 import { app } from '@/app/elysia/[[...slugs]]/route';
 
-const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000';
-
 export const elysia =
   typeof process !== 'undefined'
     ? treaty(app).elysia
-    : treaty<typeof app>(baseUrl).elysia;
+    : treaty<typeof app>('localhost:3000').elysia;
