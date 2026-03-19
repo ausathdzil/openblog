1799301329b92329da2fa0b8f7259a2eb509d153
Ausath Ikram <ausathdzil@gmail.com>
2026-03-19 19:54:10 +0700

    fix: revert eden changes (use isomorphic approach)

--- src/lib/eden.ts
+++ src/lib/eden.ts
@@ -1,5 +1,8 @@
 import { treaty } from '@elysiajs/eden';
 
-import type { app } from '@/app/elysia/[[...slugs]]/route';
+import { app } from '@/app/elysia/[[...slugs]]/route';
 
-export const elysia = treaty<typeof app>('localhost:3000').elysia;
+export const elysia =
+  typeof process === 'undefined'
+    ? treaty<typeof app>('localhost:3000').elysia
+    : treaty(app).elysia;
