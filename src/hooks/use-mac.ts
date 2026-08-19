9c226caf7c4658349fcde57515ed1e582414d41a
Ausath Ikram <ausathdzil@gmail.com>
2026-08-19 15:09:14 +0700

    style: useMac code structure

--- src/hooks/use-mac.ts
+++ src/hooks/use-mac.ts
@@ -1,23 +1,20 @@
 import { useSyncExternalStore } from 'react';
 
-function subscribe() {
-  return () => {
-    // no-op
-  };
-}
-
 const MAC_REGEX = /Mac/;
 
-function getSnapshot() {
-  return typeof window === 'undefined'
-    ? false
-    : MAC_REGEX.test(window.navigator.userAgent);
-}
-
-function getServerSnapshot() {
-  return false;
-}
-
 export function useMac() {
+  const subscribe = () => {
+    return () => {
+      // no-op
+    };
+  };
+
+  const getSnapshot = () =>
+    typeof window === 'undefined'
+      ? false
+      : MAC_REGEX.test(window.navigator.userAgent);
+
+  const getServerSnapshot = () => false;
+
   return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 }
